64455c234e01ae9c99bef81c55b3a16c6ee46392
Edgar Alexis González Amador <A01746540@itesm.mx>
2021-08-10 11:41:39 -0500

    Entrega de la Tarea 01 - Ago, Martes 10

--- Tareas/Clase01_Tarea_Brackets.py
+++ Tareas/Clase01_Tarea_Brackets.py
@@ -1,5 +1,53 @@
 # Brackets: Class 01 - Edgar Alexis González Amador - A01746540
 
+# Recibes una string conteniendo solo () y []
+# Crea un algoritmos para validar que la sintaxis de los corchetes es correcta:
+# Ejemplos validos:
+# '()'
+# '()()()()()()()()()'
+# '([])[]()'
+# Ejemplos no validos:
+# ')'
+# '()['
+# '(([))]'
+# '(((((((((((((((((((((((((((((((((('
+
+def bracketsSimpleValidation(brackets):
+    stack = []
+    for i in range(len(brackets)):
+        if brackets[i] == '(' or brackets[i] == '[':
+            stack.append(brackets[i])
+        elif brackets[i] == ')':
+            try:
+                if not (stack.pop() == '('):
+                    break
+            except:
+                stack.append(brackets[i])
+                break
+        elif brackets[i] == ']':
+            try:
+                if not (stack.pop() == '['):
+                    break
+            except:
+                stack.append(brackets[i])
+                break
+    if len(stack) != 0:
+        print('Invalid brackets :', brackets)
+    else:
+        print('Correct brackets!:', brackets)
+
+print('Examples with simple validation')
+bracketsSimpleValidation('()')
+bracketsSimpleValidation('()()()()()()()()()')
+bracketsSimpleValidation('([])[]()')
+bracketsSimpleValidation(')')
+bracketsSimpleValidation('()[')
+bracketsSimpleValidation('()[')
+bracketsSimpleValidation('(([))]')
+bracketsSimpleValidation('((((((((((((((((((((((((((((((((((')
+
+print('\n/////////////////////////////////////////////////////\n')
+
 def bracketsValidation(brackets):
     stack = []
     for i in range(len(brackets)):
@@ -22,6 +70,8 @@ def bracketsValidation(brackets):
         print('Invalid brackets')
     else:
         print('Correct brackets!!!')
+
+print('Examples step-by-step')
 
 bracketsValid = "([])()()()()"
 bracketsInvalid = "(((()(((((("
